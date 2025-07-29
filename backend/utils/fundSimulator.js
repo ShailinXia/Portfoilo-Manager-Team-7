b218588ea7c200e82e5c10495ab9f2f199397764
ShailinXia <shailinxia666@gmail.com>
2025-07-29 11:01:07 +0800

    Fix/Modify database design

--- backend/utils/fundSimulator.js
+++ backend/utils/fundSimulator.js
@@ -7,19 +7,19 @@ function formatDate(date) {
 }
 
 function simulateFundNetValue() {
-  const funds = db.prepare('SELECT fund_code, latest_net_value FROM funds').all();
+  const funds = db.prepare('SELECT code, latest_net_value FROM funds').all();
   funds.forEach(fund => {
     // 随机波动 ±1%
     const changePercent = (Math.random() * 2 - 1).toFixed(2); // -1% ~ +1%
     const newNetValue = +(fund.latest_net_value * (1 + changePercent / 100)).toFixed(4);
 
     // 更新最新净值
-    db.prepare('UPDATE funds SET latest_net_value = ? WHERE fund_code = ?')
-      .run(newNetValue, fund.fund_code);
+    db.prepare('UPDATE funds SET latest_net_value = ? WHERE code = ?')
+      .run(newNetValue, fund.code);
 
     // 插入历史净值
-    db.prepare('INSERT INTO fund_history (fund_code, date, net_value) VALUES (?, ?, ?)')
-      .run(fund.fund_code, formatDate(virtualDate), newNetValue);
+    db.prepare('INSERT INTO fund_history (code, date, net_value) VALUES (?, ?, ?)')
+      .run(fund.code, formatDate(virtualDate), newNetValue);
   });
 
   // 虚拟日期加一天
